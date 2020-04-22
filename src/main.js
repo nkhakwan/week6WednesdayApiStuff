4b73b9f93c3bc31f748fc06118bbe9eb1b71e7c0
nkhakwan <post_khan@yahoo.com>
2020-04-22 15:31:57 -0700

    more code added

--- src/main.js
+++ src/main.js
@@ -1,36 +1,44 @@
-import { Question } from './flashCards.js';
 import $ from 'jquery';
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-
-
-function displayTimer(question){
-    setInterval(() => {
-    $("#timer").html(question.score);
-   //console.log(`here is our answer ${question.response} and here is the user input ${$("#answer").val()} and here is our time ${question.time}`);
-    $("#result").html(question.response);
-},1000);
-}
-
-
-
 $(document).ready(function() {
-    let question1 = new Question();
-    $("#start-quiz").click(function(){
-        $("#question").html(question1.askQuestion());
-        question1.setScore();
-        question1.setVerification();
-        displayTimer(question1);
-    })
-    $("form#uenter").submit(function(event){
-        event.preventDefault();
-        var myAnswer = $("#answer").val();
-        console.log (`am i being defined ${myAnswer}`)
-        question1.answer = myAnswer;
-    });   
-});
+    $('#weatherLocation').click(function() {
+      const city = $('#location').val();
+      $('#location').val("");
+  
+      asyncApiCall();
+  
+      async function asyncApiCall() {
+        try {
+          let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`);
+          let jsonifiedResponse;
+          if (response.ok && response.status == 200) {
+            jsonifiedResponse = await response.json();
+          } else {
+            jsonifiedResponse = false;
+          }
+          getElements(jsonifiedResponse);
+        } catch {
+          getElements(false);
+        }
+      }
+  
+      const getElements = function(response) {
+        if (response) {
+          $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+          $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+        } else {
+          $('.showHumidity').text(`There was an error handling your request.`);
+          $('.showTemp').text(`Please check your inputs and try again!`);
+        }
+      }
+    });
+  });
+
+
+
 
 
 
