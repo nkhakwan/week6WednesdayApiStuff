26be060c2cbb1ca94098ef2b9622ce8d3101d1df
nkhakwan <post_khan@yahoo.com>
2020-04-22 18:59:25 -0700

    more modifications made in the files

--- src/dino.js
+++ src/dino.js
@@ -1,41 +1,3 @@
-export class Question {
-    constructor(){
-        this.answer = "";
-        this.score = 10;
-        this.response = "";
-        this.time = "";
-    }
-
-  setVerification() {
-    setTimeout(() => {
-      console.log(`Here is the answer that the function got ${this.answer}`);
-      let answers =["blue"];
-      if (answers[0] === this.answer){
-        this.response = "correct";
-      }else {
-        this.response = "not correct";
-      }    
-      
-    }, 10000);
-  }
-
-  setScore() {
-    let ggg = setInterval(() => {
-      this.score --;
-      if(this.answer != ""){
-        this.time = this.score;
-        clearInterval(ggg);
-      }
-      console.log (`input was inputted at ${this.time}`);
-    }, 1000);
-  }
-
-  askQuestion(){
-    return "what is the coolest color";
-  }
-  
-}
-
 import {Dino} from '../src/dino.js'
 
 describe('Initial Dino-Ipsum Calls', () => {
@@ -90,3 +52,47 @@ export class Dino{
     }
   }
 }
+
+
+//==============================================================================
+//==============================================================================
+//==============================================================================
+
+
+/*export class Question {
+  constructor(){
+      this.answer = "";
+      this.score = 10;
+      this.response = "";
+      this.time = "";
+  }
+
+setVerification() {
+  setTimeout(() => {
+    console.log(`Here is the answer that the function got ${this.answer}`);
+    let answers =["blue"];
+    if (answers[0] === this.answer){
+      this.response = "correct";
+    }else {
+      this.response = "not correct";
+    }    
+    
+  }, 10000);
+}
+
+setScore() {
+  let ggg = setInterval(() => {
+    this.score --;
+    if(this.answer != ""){
+      this.time = this.score;
+      clearInterval(ggg);
+    }
+    console.log (`input was inputted at ${this.time}`);
+  }, 1000);
+}
+
+askQuestion(){
+  return "what is the coolest color";
+}
+
+}*/
--- src/main.js
+++ src/main.js
@@ -1,26 +1,31 @@
-import $ from 'jquery';
+//import $ from 'jquery';
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+
 
 $(document).ready(function() {
     $('#weatherLocation').click(function() {
       const city = $('#location').val();
       $('#location').val("");
-  
+        console.log(`logger is in read function with city as ${city}`);
       asyncApiCall();
   
       async function asyncApiCall() {
         try {
-          let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`);
+          let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8f9c58064d1479fa6f4baa7bef6415fc`);
           let jsonifiedResponse;
           if (response.ok && response.status == 200) {
             jsonifiedResponse = await response.json();
+            console.log(`logger is in correct response arena`);
           } else {
             jsonifiedResponse = false;
+            console.log(`logger is in false arena`);
+            console.log(`logger asking for result and here it is ${jsonifiedResponse}`)
           }
           getElements(jsonifiedResponse);
-        } catch {
+        } catch(error) {
           getElements(false);
         }
       }
